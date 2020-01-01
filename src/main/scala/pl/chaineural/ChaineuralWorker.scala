438c8d98a835189374e2c59ae069b6fccf42dad9
devmood <devmood.business@gmail.com>
2020-01-02 00:59:14 +0100

    Fixed forward pass in the single-worker network; almost done with backward pass
    Need to check the asserts and make shapes fit well, to ensure we retrieve what we expect

--- src/main/scala/pl/chaineural/ChaineuralWorker.scala
+++ src/main/scala/pl/chaineural/ChaineuralWorker.scala
@@ -52,38 +52,56 @@ class ChaineuralWorker(stalenessWorker: ActorRef, amountOfWorkers: Int) extends 
   override def receive: Receive = initializeWeights
 
   def initializeWeights: Receive = {
+    /**
+     * Duplicated with the processWork receive handler,
+     * but for now (due to the state initialization) let's leave it as is.
+     */
     case up2DateParametersAndStaleness: Up2DateParametersAndStaleness =>
-      processWork(up2DateParametersAndStaleness)
+      log.info("[worker]: Up to date parameters and staleness (initialization)")
+      context become processWork(up2DateParametersAndStaleness)
   }
 
   def processWork(up2DateParametersAndStaleness: Up2DateParametersAndStaleness): Receive = {
-    case Up2DateParametersAndStaleness =>
-      processWork(up2DateParametersAndStaleness)
+    case up2DateParametersAndStaleness: Up2DateParametersAndStaleness =>
+      log.info("[worker]: Up to date parameters and staleness (update)")
+      context become processWork(up2DateParametersAndStaleness)
 
     case ForwardPass(x: M, y: V) =>
-      log.info(s"Gotten work of size ${x.size}")
-      log.info(s"input shape: ${x.size}, ${x.head.size}")
+      log.info(s"[worker]: Forward pass")
+      log.info(s"[worker]: The input's shape is (${x.size}, ${x.head.size})")
 
       val θZ1: Matrices = Matrices(Matrices(Matrices(x) ⓧ up2DateParametersAndStaleness.θW1) + up2DateParametersAndStaleness.θB1)
-      // val θA1: M = tanh(θZ1)
-      val θA1: Matrices = θZ1
+      val θA1: Matrices = Matrices(!θZ1)
       val θZ2: Vectors = Vectors(Matrices(Matrices(θA1 ⓧ up2DateParametersAndStaleness.θW2) + up2DateParametersAndStaleness.θB2).squeeze())
       val Loss: Float = 1.0f / amountOfWorkers.toFloat * math.pow(Vectors(θZ2 - y).sumValues, 2.0f).toFloat
 
-      self ! BackwardPass(θZ1, θA1, θZ2, Loss)
+      log.info(f"[worker]: Loss = $Loss%1.8f")
 
-    case BackwardPass(zθ1: Matrices, aθ1: Matrices, zθ2: Vectors, loss: Float) =>
+      self ! BackwardPass(up2DateParametersAndStaleness.amountOfMiniBatches, x, y, θZ1, θA1, θZ2, Loss)
+
+    case BackwardPass(amountOfMiniBatches: Int, X: M, Y: V, zθ1: Matrices, aθ1: Matrices, zθ2: Vectors, loss: Float) =>
+      log.info(s"[worker]: Backward pass")
       val dθLoss: Float = 1.0f
-      val JacobianθZ2: M = Vector(Vector())
-      val JacobianθB2: M = Vector(Vector())
-      val JacobianθA1: M = Vector(Vector())
-      val JacobianθW2: M = Vector(Vector())
-      val JacobianθZ1: M = Vector(Vector())
-      val JacobianθW1: M = Vector(Vector())
-      val JacobianθB1: M = Vector(Vector())
+      val dLossdθZ2: Float = 2.0f / amountOfMiniBatches * Vectors(zθ2 - Y).sumValues
+      val JacobianθZ2: V = zθ2 * dLossdθZ2
+      val JacobianθB2: V = JacobianθZ2 // this one needs to be expanded into matrix
+      val JacobianθW2: M = aθ1 * dLossdθZ2
+      val tmp: M = Matrices(!zθ1).elementWisePow(2)
+      val dθZ2dθA1: Float = Matrices(Matrices(tmp).ones) - (Matrices(!zθ1).elementWisePow(2))
+      val JacobianθZ1: M = zθ1 * (dLossdθZ2 * dθZ2dθA1)
+      val JacobianθB1: M = JacobianθZ1
+      val JacobianθW1: M = Matrices(X) * (dLossdθZ2 * dθZ2dθA1)
+
+      assert(testShapes(JacobianθW1, up2DateParametersAndStaleness.θW1))
+      assert(testShapes(JacobianθB1, up2DateParametersAndStaleness.θB1))
+      assert(testShapes(JacobianθW2, up2DateParametersAndStaleness.θW2))
+      assert(testShapes(JacobianθB2, up2DateParametersAndStaleness.θB2))
 
       stalenessWorker ! BackPropagatedParameters(JacobianθW1, JacobianθB1, JacobianθW2, JacobianθB2)
 
     case _ =>
   }
+
+  def testShapes(jacobian: M, matrix: M): Boolean =
+    jacobian.size == matrix.size && jacobian(0).size == matrix(0).size
 }
